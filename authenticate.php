14eccf3843bfbf08f638decc87594ca1c7b195cd
Kevyn Hale <kahale.ilo@gmail.com>
2016-01-02 07:10:23 -0700

    hiding info

--- authenticate.php
+++ authenticate.php
@@ -5,19 +5,8 @@ session_start();
 function authenticate($id, $token_id, $name, $email, $image) {
     if(empty($id) || empty($token_id)) return false;
 
-########## MySql details  #############
-$db_username = "2018160_cur"; //Database Username
-$db_password = "curriculum123"; //Database Password
-$host_name = "pdb6.runhosting.com"; //Mysql Hostname
-$db_name = '2018160_cur'; //Database Name
-###################################################################
+include("php/dbConnect.php");
 
- // connect to database
-$mysqli = new mysqli($host_name, $db_username, $db_password, $db_name);
-        if ($mysqli->connect_error) {
-        die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
-        }
-        
 //check if user exist in database using COUNT
     $result = $mysqli->query("SELECT COUNT(id) as usercount FROM curriculum_users WHERE id='".$id."'");
     
--- php/dbConnect.php
+++ php/dbConnect.php
@@ -1,11 +1,14 @@
 <?php
 
-########## MySql details  #############
-$db_username = "2018160_cur"; //Database Username
-$db_password = "curriculum123"; //Database Password
-$host_name = "pdb6.runhosting.com"; //Mysql Hostname
-$db_name = '2018160_cur'; //Database Name
-###################################################################
+$filename = "../dbconnect-info.php";
+
+if (file_exists($filename)) {
+     include("../dbconnect-info.php");
+ } else {
+     include("../../dbconnect-info.php");
+ }
+
+
 
  // connect to database
 $mysqli = new mysqli($host_name, $db_username, $db_password, $db_name);
