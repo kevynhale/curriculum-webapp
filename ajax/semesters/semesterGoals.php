cc5c84e795a724ef6c5d2f222fc534899e55b803
Kevyn Hale <kahale.ilo@gmail.com>
2016-01-22 06:17:33 -0700

    progress on sem goals

--- ajax/semesters/semesterGoals.php
+++ ajax/semesters/semesterGoals.php
@@ -1,4 +1,17 @@
-<?php
-echo 'these are the goals';
+<div id="semester-goals-header">
+	<ul id="semester-goals-bullet">
+		<li id="semester-goals-goal" class="semester-goals-item">
+		Goal
+		</li>
+		<li id="semester-goals-status" class="semester-goals-item">
+		Status
+		</li>
+		<li id="semester-goals-add" class="semester-goals-item">
+		+
+		</li>
+	</ul>
+</div> <!-- end of div semester-goals-header -->
 
-?>
+<div id="semester-goals-body">
+
+</div> <!-- end of div semester-goals-body -->
